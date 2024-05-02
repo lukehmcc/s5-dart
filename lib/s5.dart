775bc6dcd0da74abd62a92c4e019050bc4401bd6
redsolver <info@redsolver.net>
2024-05-02 22:15:23 +0200

    Add logger

--- lib/s5.dart
+++ lib/s5.dart
@@ -2,5 +2,6 @@
 library;
 
 export 'src/s5_base.dart';
+export 'src/logger.dart';
 
 export 'package:lib5/lib5.dart';
---
+++ lib/src/logger.dart
@@ -0,0 +1,9 @@
+import 'package:lib5/util.dart';
+
+class S5Logger extends SimpleLogger {
+  S5Logger({
+    super.prefix = '[S5] ',
+    super.format = true,
+    super.showVerbose = false,
+  });
+}
